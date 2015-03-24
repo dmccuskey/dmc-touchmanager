02d9bb3d31b405237bf1c4727418aee9b8b778e9
dmc <my-github@davidmccuskey.com>
2015-03-24 04:08:41 -0600

    update touchmanager basic example

--- examples/dmc-touchmanager-basic/config.lua
+++ examples/dmc-touchmanager-basic/config.lua
@@ -1,14 +1,14 @@
 application =
 {
-    content =
-    {
-        width = 768,
-        height = 1024,
-        scale = "letterbox",
+	content =
+	{
+		width = 320,
+		height = 480,
+		scale = "letterbox",
 
-        imageSuffix =
-        {
-            ["@2x"] = 2,    -- images with "@2x" appended will be used for iPad 3
-        }
-    }
+		imageSuffix =
+		{
+			["@2x"] = 2
+		}
+	}
 }
--- examples/dmc-touchmanager-basic/main.lua
+++ examples/dmc-touchmanager-basic/main.lua
@@ -3,118 +3,149 @@
 --
 -- Shows simple use of the Touch Manager
 --
--- by David McCuskey
---
 -- Sample code is MIT licensed, the same license which covers Lua itself
 -- http://en.wikipedia.org/wiki/MIT_License
--- Copyright (C) 2012 David McCuskey. All Rights Reserved.
+-- Copyright (C) 2012-2015 David McCuskey. All Rights Reserved.
 --====================================================================--
 
 
-print("---------------------------------------------------")
+
+print( '\n\n##############################################\n\n' )
 
 
---===================================================================--
--- Imports
---===================================================================--
 
-local TouchMgr = require( "dmc_touchmanager" )
+--====================================================================--
+--== Imports
 
 
--- Tools and Debugging
---[[
-local isSimulator = "simulator" == system.getInfo("environment")
-if isSimulator then
-	ultimote = require "Ultimote"
-	ultimote.connect()
-end
---]]
+local TouchMgr = require 'dmc_corona.dmc_touchmanager'
 
 
---===================================================================--
--- Setup, Constants
---===================================================================--
+
+--====================================================================--
+--== Setup, Constants
+
+
+local W, H = display.contentWidth, display.contentHeight
+local H_CENTER, V_CENTER = W*0.5, H*0.5
 
 display.setStatusBar( display.HiddenStatusBar )
 
 
---===================================================================--
--- Main
---===================================================================--
+
+--====================================================================--
+--== Support Functions
 
 
--- createTouchPoint()
--- create display object used to represent a screen touch
+--- create Display Object used to represent a screen touch
 --
--- @params color table with R,G,B values
+-- @table color (array) with R,G,B values
 -- @return instance of display object
 --
-function createTouchPoint( color )
-	local o = display.newCircle( 0, 0, 40 )
+local function createTouchPoint( color )
+	local o = display.newCircle( 0, 0, 20 )
 	o:setFillColor( unpack( color ) )
 	return o
 end
 
 
+--- create a regular Display Object for touch area
+-- add some properties used for touch management
+--
+-- @table fColor (array) with R,G,B values, fill color
+-- @return instance of display object
+--
+local function createDisplayObject( fColor )
 
---== Setup Object-type Handler ==--
-
-function setup_object_example()
-
-	-- setup display object
-	local color = { 255, 255, 255 }
-	local o = display.newRect( 0, 0, 300, 300 )
-	o:setFillColor( unpack( color ) )
+	local o = display.newRect( 0, 0, 150, 150 )
+	o:setFillColor( unpack( fColor ) )
 	o.alpha = 0.5
-	o:setReferencePoint( display.CenterReferencePoint )
-	o.x = 384 ; o.y = 275
 
-	-- setup some vars used to track touches
-	local tp
+	--== setup vars ON display object to track touches
+
+	-- active number of touches for our shape
 	o._touch_num = 0
-	o.touches = {}
+
+	-- table holding visual touch points, hashed by touch-event id
+	o._touch_points = {}
+
+	return o
+end
 
 
-	-- setup our object-type event call
-	--
+--== Helper functions, showing activity
+
+local function updateDisplayObject( o, sColor, sWidth )
+	o:setStrokeColor( unpack( sColor ) )
+	o.strokeWidth = sWidth
+end
+
+local function highlightDisplayObject( o )
+	updateDisplayObject( o, { 0.7, 0.7, 0.7 }, 10 )
+end
+
+local function unhighlightDisplayObject( o )
+	updateDisplayObject( o, { 0, 0, 0 }, 0 )
+end
+
+
+
+--======================================================--
+-- Setup Object-type Handler
+
+local function setup_object_example()
+
+	--== setup our display object
+
+	local color = { 1, 1, 1 }
+	local o = createDisplayObject( color )
+	o.x, o.y = H_CENTER, V_CENTER-115
+
+
+	--== setup our object-type event call
+
 	function o:touch( event )
+		-- print( "o:touch", event.phase, event.id )
+		local tp -- visual touch point
 
 		if event.phase == 'began' then
 
-			TouchMgr:setFocus( self, event.id )
-
-			self:setStrokeColor( 171, 171, 255 )
-			self.strokeWidth = 10
-
-			self._touch_num = self._touch_num + 1
+			highlightDisplayObject( o )
 
 			tp = createTouchPoint( color )
-			tp.x = event.x ; tp.y = event.y
-			self.touches[ event.id ] = tp
+			tp.x, tp.y = event.x, event.y
+			self._touch_points[ event.id ] = tp
+			self._touch_num = self._touch_num + 1
 
+			TouchMgr.setFocus( self, event.id )
 
-		elseif event.phase == 'moved' then
+			return true
+		end
 
-			tp = self.touches[ event.id ]
+		if not event.isFocused then return end
+
+		if event.phase == 'moved' then
+
+			tp = self._touch_points[ event.id ]
 			if tp then
-				tp.x = event.x ; tp.y = event.y
+				tp.x, tp.y = event.x, event.y
 			end
 
 
-		elseif ( event.phase == 'canceled' or event.phase == 'ended' ) then
+		elseif ( event.phase=='ended' or event.phase=='cancelled' ) then
 
-			TouchMgr:unsetFocus( self, event.id )
+			TouchMgr.unsetFocus( self, event.id )
 
-			tp = self.touches[ event.id ]
+			tp = self._touch_points[ event.id ]
+			assert( tp, 'tp not found', event.id )
 			if tp then
 				self._touch_num = self._touch_num - 1
-				self.touches[ event.id ] = nil
+				self._touch_points[ event.id ] = nil
 				tp:removeSelf()
 			end
 
 			if self._touch_num == 0 then
-				self:setStrokeColor( 0, 0, 0 )
-				self.strokeWidth = 0
+				unhighlightDisplayObject( o )
 			end
 
 		end
@@ -123,113 +154,109 @@ function setup_object_example()
 	end
 
 
-	-- return reference back for Touch Manager initialization
+	-- return object reference back for Touch Manager initialization
 	return o
-
 end
 
 
 
---== Setup Function-type Handler ==--
+--======================================================--
+-- Setup Function-type Handler
 
-function setup_function_example()
+local function setup_function_example()
 
-	-- setup display object
-	local color = { 255, 0, 0 }
-	local o = display.newRect( 0, 0, 300, 300 )
-	o:setFillColor( unpack( color ) )
-	o.alpha = 0.5
-	o:setReferencePoint( display.CenterReferencePoint )
-	o.x = 384 ; o.y = 725
+	--== setup display object
 
-	-- setup some vars used to track touches
-	local tp
-	o._touch_num = 0
-	o.touches = {}
+	local color = { 1, 0, 0 }
+	local o = createDisplayObject( color )
+	o.x, o.y = H_CENTER, V_CENTER+115
 
 
-	-- setup our function-type event call
-	--
+	--== setup our function-type event call
+
 	local handler = function( event )
+		-- print( "handler", event, event.phase )
+		local target = event.target -- our display object, 'o'
 
-		local target = event.target
+		if event.phase=='began' then
 
-		if event.phase == 'began' then
-
-			TouchMgr:setFocus( target, event.id )
-
-			target:setStrokeColor( 171, 171, 255 )
-			target.strokeWidth = 10
-
-			target._touch_num = target._touch_num + 1
+			highlightDisplayObject( target )
 
 			tp = createTouchPoint( color )
-			tp.x = event.x ; tp.y = event.y
-			target.touches[ event.id ] = tp
+			tp.x, tp.y = event.x, event.y
+			target._touch_points[ event.id ] = tp
+			target._touch_num = target._touch_num + 1
 
+			TouchMgr.setFocus( target, event.id )
 
-		elseif event.phase == 'moved' then
+			return true
+		end
 
-			tp = target.touches[ event.id ]
+		if not event.isFocused then return end
+
+		if event.phase=='moved' then
+
+			tp = target._touch_points[ event.id ]
 			if tp then
-				tp.x = event.x ; tp.y = event.y
+				tp.x, tp.y = event.x, event.y
 			end
 
 
-		elseif ( event.phase == 'canceled' or event.phase == 'ended' ) then
+			elseif ( event.phase=='ended' or event.phase=='cancelled' ) then
 
-			TouchMgr:unsetFocus( target, event.id )
+			TouchMgr.unsetFocus( target, event.id )
 
-			tp = target.touches[ event.id ]
+			tp = target._touch_points[ event.id ]
+			assert( tp, 'tp not found', event.id )
 			if tp then
 				target._touch_num = target._touch_num - 1
-				target.touches[ event.id ] = nil
+				target._touch_points[ event.id ] = nil
 				tp:removeSelf()
 			end
 
 			if target._touch_num == 0 then
-				target:setStrokeColor( 0, 0, 0 )
-				target.strokeWidth = 0
+				unhighlightDisplayObject( target )
 			end
 
 		end
 
-		return true 
+		return true
 	end
-
 
 	-- return references back for Touch Manager initialization
 	return o, handler
-
 end
 
 
 
--- main()
---
-local main = function()
+--====================================================================--
+--== Main
+--====================================================================--
+
+
+local function main()
 
 	local o, h
-	
+
+	-- register the object as OO with the Touch Manager
+
 	o = setup_object_example()
+	TouchMgr.register( o )
 
-	-- register the object/ handler with the Touch Manager
-	--
-	TouchMgr:register( o )
-
+	-- register the object/handler with the Touch Manager
 
 	o, h = setup_function_example()
+	TouchMgr.register( o, h )
 
-	-- register the object/ handler with the Touch Manager
-	--
-	TouchMgr:register( o, h )
+	timer.performWithDelay( 20000, function()
+		TouchMgr.unregister( o, h )
+	end)
 
 end
 
 
--- let's get this party started !
---
-main()
+-- start the action !
 
+main()
 
 
